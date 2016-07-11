76e4025da2b91673fe095f3732665bba7247e5be
thash <hash@hzl.jp>
2016-07-11 20:49:25 +0900

    L/R GUI, copy/cut/paste key

--- keyboard/ergodox_ez/keymaps/memerelics/keymap.c
+++ keyboard/ergodox_ez/keymaps/memerelics/keymap.c
@@ -12,19 +12,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  ESC   |   1  |   2  |   3  |   4  |   5  |   6  |           |   7  |   8  |   9  |   0  |   -  |   =  | bACkSp |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |   (  |           |   )  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | cut  |           | copy |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '    |
- * |--------+------+------+------+------+------|   [  |           |   ]  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|paste |           | undo |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- * | Grv/L1 |  '"  |  Alt |  App |  Alt |                                       | Alt  | Down |   [  |   ]  | ~L1  |
+ * | Grv/L1 |  '"  |  Alt |  App | LGui |                                       | RGui | Alt  |   [  |   ]  | ~L1  |
  * `------------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Left |Right |       | Home | End  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Up   |       | PgUp |        |      |
- *                                 |Space | LGui |------|       |------| RGui   |Enter |
+ *                                 |Space | Alt  |------|       |------| Alt    |Enter |
  *                                 |      |      | Down |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -33,26 +33,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LPRN,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LALT(KC_X),
         KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   LALT(KC_V),
 
-        LT(SYMB,KC_GRV),KC_QUOT,   KC_LALT,  ALT_T(KC_APP), KC_LALT,
+        LT(SYMB,KC_GRV),KC_QUOT,   KC_LALT,  ALT_T(KC_APP), KC_LGUI,
 
                                                             KC_LEFT,  KC_RGHT,
                                                                         KC_UP,
-                                                       KC_SPC,KC_LGUI,KC_DOWN,
+                                                       KC_SPC,KC_LALT,KC_DOWN,
         // right hand
              KC_7,        KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
-             KC_RPRN,     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,             KC_BSLS,
+             LALT(KC_C),  KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,             KC_BSLS,
                           KC_H,   KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN),KC_QUOT,
-             KC_RBRC,     KC_N,   KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),   KC_RSFT,
+             LALT(KC_Z),  KC_N,   KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),   KC_RSFT,
 
-                                           KC_RALT,  KC_DOWN, KC_LBRC, KC_RBRC, KC_FN1,
+                                           KC_RGUI,  KC_RALT, KC_LBRC, KC_RBRC, KC_FN1,
 
              KC_HOME, KC_END,
              KC_PGUP,
-             KC_PGDN, KC_RGUI, KC_ENT
+             KC_PGDN, KC_RALT, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
